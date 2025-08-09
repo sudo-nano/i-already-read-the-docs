cee82dd3d2ae52802ad592d6873de208c8713db0
Connor <cwg1231@gmail.com>
2025-08-09 16:58:47 -0700

    Updated project name and copyright info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'IAlreadyReadTheDocs'
+copyright = '2025, TheMagicalC'
+author = 'TheMagicalC'
 
 release = '0.1'
 version = '0.1.0'
